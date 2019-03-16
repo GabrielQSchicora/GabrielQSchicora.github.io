8c07ef7f049e63a45523fce09edcb1ed8eb813b2
gabrielqschicora <gabriel.q.s@live.com>
2019-03-15 23:43:20 -0300

    Change About Me

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,3 +1,3 @@
 var idade = getDateDiff(new Date('1998-12-26'), new Date('2018-12-26'), 'years');
 
-$('div.about-me div.text').text('Meu nome é Gabriel, tenho ' + idade + ' anos, sou desenvolvedor com conhecimento em HTML, CSS, Javascript, e bibliotecas como o JQuery e o SASS, atuando também com Back-End na linguagem PHP e com conhecimento básico ou intermediário em SQL, MySql, Java, C#, Android, Python e Metodologias Ágeis. Sou grande entusiasta na área da Inteligência Artificial e Machine Learning.')
+$('div.about-me div.text').text('Meu nome é Gabriel, tenho ' + idade + ' anos, sou desenvolvedor com conhecimento em HTML, CSS, Javascript, e bibliotecas como o JQuery e o SASS, atuando também com Back-End na linguagem PHP e com conhecimento básico e intermediário em SQL, MySql, Java, C#, Android, Python e Metodologias Ágeis.')
