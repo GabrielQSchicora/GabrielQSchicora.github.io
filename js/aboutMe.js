fbe3c97cfdb8ce0529fc9b02816a22049cbea0a2
gabrielqschicora <gabriel.q.s@live.com>
2019-03-20 19:33:05 -0300

    Added Search Console Verification

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,3 +1,3 @@
-var idade = getDateDiff(new Date('1998-12-26'), new Date('2018-12-26'), 'years');
+var idade = getDateDiff(new Date('1998-12-26'), new Date(), 'years');
 
 $('div.about-me div.text').text('Meu nome é Gabriel, tenho ' + idade + ' anos, sou desenvolvedor com conhecimento em HTML, CSS, Javascript, e bibliotecas como o JQuery e o SASS, atuando também com Back-End na linguagem PHP e com conhecimento básico e intermediário em SQL, MySql, Java, C#, Android, Python e Metodologias Ágeis.')
